1e3e62a2f2873628391b7eb84782f65ceb0c3fd1
hbazai <bazaee.hassan@gmail.com>
2024-06-30 08:18:46 +0330

    supabase login

--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,51 @@
 <template>
-    <div>
-        <p>login page</p>
+  <div>
+    <label>email</label>
+    <br />
+    <input type="email" v-model="email" />
+    <br />
+    <br />
+    <label>password</label>
+    <br />
+    <input type="password" v-model="password" />
+
+    <div v-if="!successMsg">
+      <button @click="login">Login</button>
     </div>
+    <div v-else>
+      <p v-if="errorMsg">Error!!</p>
+      <p v-else>Success!!</p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+  const supabase = useSupabaseClient();
+  const user = useSupabaseUser();
+  const email = useState(() => "");
+  const password = useState(() => "");
+  const successMsg = useState(() => false);
+  const errorMsg = useState(() => false);
 
+  if (user.value) await navigateTo("/dashboard");
+
+const login = async () => {
+  const {data, error} = await supabase.auth.signInWithPassword({
+    email:email.value,
+    password:password.value,
+  })
+
+  if(error) {
+      successMsg.value = false
+      errorMsg.value = true
+      return
+    }
+
+    errorMsg.value = false
+    successMsg.value = true
+    setTimeout(async ()=>{
+      successMsg.value = false
+      await navigateTo('/dashboard')
+    },2000)
+}
 </script>
-
-<style scoped>
-
-</style>
